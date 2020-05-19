0ada34c20462be745618babe1361e33a15ce1883
angelorobson <helloween123>
2020-05-19 11:18:41 -0300

    Created userDetailsUpdateTest

---
+++ app/src/test/java/br/com/angelorobson/usermvi/usersdetails/UserDetailsUpdateTest.kt
@@ -0,0 +1,94 @@
+package br.com.angelorobson.usermvi.usersdetails
+
+import br.com.angelorobson.usermvi.model.User
+import br.com.angelorobson.usermvi.userdetails.*
+import com.spotify.mobius.test.NextMatchers.*
+import com.spotify.mobius.test.UpdateSpec
+import com.spotify.mobius.test.UpdateSpec.assertThatNext
+import io.mockk.every
+import io.mockk.mockkStatic
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+class UserDetailsUpdateTest {
+
+    private lateinit var updateSpec: UpdateSpec<UserDetailModel, UserDetailEvent, UserDetailEffect>
+
+
+    @Before
+    fun setUp() {
+        updateSpec = UpdateSpec(::userDetailUpdate)
+    }
+
+    @Test
+    fun InitialEvent_DispatchLoadUser() {
+        mockkStatic("br.com.angelorobson.usermvi.userdetails.UserDetailViewModelKt")
+        every { isOnline() } returns true
+
+        val model = UserDetailModel(isLoading = true)
+        val id = 1
+
+        updateSpec
+            .given(model)
+            .whenEvent(Initial(id))
+            .then(
+                assertThatNext(
+                    hasModel(model.copy(isLoading = true)),
+                    hasEffects(LoadUser(id) as UserDetailEffect)
+                )
+            )
+    }
+
+    @Test
+    fun InitialEvent_DispatchLoadUserLocally() {
+        mockkStatic("br.com.angelorobson.usermvi.userdetails.UserDetailViewModelKt")
+        every { isOnline() } returns false
+
+        val model = UserDetailModel(isLoading = true)
+        val id = 1
+
+        updateSpec
+            .given(model)
+            .whenEvent(Initial(id))
+            .then(
+                assertThatNext(
+                    hasModel(model.copy(isLoading = true)),
+                    hasEffects(LoadUserLocally(id) as UserDetailEffect)
+                )
+            )
+    }
+
+    @Test
+    fun LoadUserEvent_HasModelWithUSer() {
+        val user = User(1, "name", "username")
+        val model = UserDetailModel(user = user)
+
+        updateSpec
+            .given(model)
+            .whenEvent(UserLoaded(user))
+            .then(
+                assertThatNext(
+                    hasModel(model.copy(isLoading = false, user = user)),
+                    hasNoEffects()
+                )
+            )
+    }
+
+    @Test
+    fun ErrorLoadingUSer_HasModelWithError() {
+        val model = UserDetailModel()
+
+        updateSpec
+            .given(model)
+            .whenEvent(ErrorLoadingUSer)
+            .then(
+                assertThatNext(
+                    hasModel(model.copy(error = true, isLoading = false)),
+                    hasNoEffects()
+                )
+            )
+    }
+
+}
